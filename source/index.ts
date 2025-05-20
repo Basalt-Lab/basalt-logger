83f2ad86769cd4e393280b9eab697c209f375516
ruby <necrelox@proton.me>
2025-05-20 16:14:50 +0200

    build(📦): [Fix export]

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,20 @@
+// Enums
+export * from './enums/loggerErrorKeys';
+
+// Error
+export * from './error/basaltError';
+export type * from './error/types/basaltErrorOptions';
+
+// Strategies
+export * from './strategies/consoleLogger';
+export * from './strategies/fileLogger';
+
+// Types
+export type * from './types/bodiesIntersection';
+export type * from './types/loggerStrategy';
+export type * from './types/logLevels';
+export type * from './types/logStreamChunk';
+export type * from './types/strategyBody';
+export type * from './types/strategyMap';
+
 export * from './basaltLogger';
-export * from './enums/index';
-export * from './error/index';
-export type * from './error/types/index';
-export * from './strategies/index';
-export type * from './types/index';
